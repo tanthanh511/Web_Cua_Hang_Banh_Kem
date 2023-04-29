58acc2158816a8c70a2b62cd641fdb8896722f70
Tôi Tên Du và Du Là Tôi <106806693+baolach123@users.noreply.github.com>
2023-04-30 05:29:39 +0700

    Them giao dien cho Shop va detail product

---
+++ CakeShop/CakeShop/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CakeShop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CakeShop.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly CuaHangBanhKemContext _context;
+
+        public ProductController(CuaHangBanhKemContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(int id)
+        {
+            var product = _context.Products.Include(x => x.CatId).FirstOrDefault(x => x.ProductId == id);
+            if (product == null)
+            {
+                return RedirectToAction("Index");
+            }
+            
+            return View(product);
+        }
+    }
+}
